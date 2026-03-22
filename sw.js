4dbc24088f537fe5276a9fac9cf1bd8f43fd4f66
janithdidula025-design <janithdidula025@gmail.com>
2026-03-22 23:16:15 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,31 @@
-const CACHE_NAME = 'adm-v1';
-const assets = ['./', './index.html', './admin.html', './manifest-card.json', './manifest-admin.json'];
+const CACHE_NAME = 'adm-vcard-v2';
+const ASSETS = [
+    '/',
+    '/index.html',
+    '/admin.html',
+    '/manifest-card.json',
+    '/manifest-admin.json'
+];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys => Promise.all(
+            keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null)
+        ))
+    );
+});
+
+self.addEventListener('fetch', event => {
+    // Cache-First strategy for application assets
+    event.respondWith(
+        caches.match(event.request).then(cachedResponse => {
+            return cachedResponse || fetch(event.request);
+        })
+    );
 });
