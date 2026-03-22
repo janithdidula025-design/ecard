e067bda5483a212b5db0cf075a9d64cf1d947a77
janithdidula025-design <janithdidula025@gmail.com>
2026-03-22 23:07:07 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,10 @@
-self.addEventListener('fetch', function(event) {});
+const CACHE_NAME = 'adm-v1';
+const assets = ['./', './index.html', './admin.html', './manifest-card.json', './manifest-admin.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
