f40b71c83e2aee6de2b12e5a113f344693546f53
janithdidula025-design <janithdidula025@gmail.com>
2026-03-24 00:03:03 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,14 @@
-const CACHE_NAME = 'adm-vcard-v2';
+const CACHE_NAME = 'adm-v4';
 const assets = [
   './index.html',
   './admin.html',
-  'https://raw.githubusercontent.com/janithdidula025-design/adm-vcard/main/logo.jpg'
+  './sw.js'
 ];
 
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
