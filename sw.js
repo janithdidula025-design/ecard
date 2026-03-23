9eba2b075f4b8a1ef03b5a926e4caf602b82e7ae
janithdidula025-design <janithdidula025@gmail.com>
2026-03-23 19:21:42 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,2 @@
-const CACHE_NAME = 'adm-vcard-v3';
-const assets = ['./', './index.html', './admin.html', './manifest-card.json', './manifest-admin.json'];
-
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(assets)));
-});
-
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
-});
+self.addEventListener('install', (e) => { self.skipWaiting(); });
+self.addEventListener('fetch', (e) => { e.respondWith(fetch(e.request)); });
