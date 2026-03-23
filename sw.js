2ff315991039f2cc96a76dd171a169be2b4629b6
janithdidula025-design <janithdidula025@gmail.com>
2026-03-23 23:22:29 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,36 @@
-const CACHE_NAME = 'adm-v1';
-const ASSETS = [
+const CACHE_NAME = 'adm-admin-v2';
+// මෙහි ඔබගේ ප්‍රධාන HTML ගොනුවේ නම නිවැරදි දැයි බලන්න (e.g. admin.html)
+const assets = [
   './admin.html',
-  './index.html'
+  'https://raw.githubusercontent.com/janithdidula025-design/adm-vcard/main/logo.jpg'
 ];
 
-// Install Service Worker
-self.addEventListener('install', (e) => {
-  e.waitUntil(
+// Service Worker එක Install වන විට අවශ්‍ය දේවල් Cache කිරීම
+self.addEventListener('install', (event) => {
+  event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
+      console.log('Caching assets...');
+      return cache.addAll(assets);
     })
   );
 });
 
-// Active Service Worker
-self.addEventListener('activate', (e) => {
-  console.log('ADM Service Worker Active');
+// පැරණි Cache දත්ත ඉවත් කර අලුත් Version එක Activate කිරීම
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
 });
 
-// Fetch events
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
+// Offline අවස්ථාවකදී පවා App එක වැඩ කිරීමට අවශ්‍ය දත්ත ලබා දීම
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
