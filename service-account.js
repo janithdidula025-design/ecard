7024eeed00f9e06fa72abd710e7bdd7a1d652a7b
janithdidula025-design <janithdidula025@gmail.com>
2026-03-31 21:14:39 +0530

    Update and rename firebase-messaging-sw.js to service-account.js

---
+++ service-account.js
@@ -0,0 +1,13 @@
+{
+  "type": "service_account",
+  "project_id": "ecard-c8f61",
+  "private_key_id": "3ee49a25c7ff8db06c70622de9df887eab476acd",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQCqYnwsivRvC2Qf\nspKFBQOGjJG8fyU1FascvaeRwExKhouBSyyutAwuXj0CDz2mDOpmySZ+hiqotXui\nEM5vWYOtEtfw2pnhA85mvVJsS8jMAhx/NfXpcfc3mj95lXIfqRyAULM+03x7jrJF\nsDOYrzpeTc166F0xGUgzq6IDC01S7wef+blwOdJCFe8yg2MlUQk/vKAHDy5CzdVW\nR3YXI1lawadouCOVQyNDrFP/DelclHZrI0yK46LsN5Q8A6AIO5pgDYlp14u4Tlnm\nn/+67enyle1Dkxv28Y/wUbkLpBoPkztUTaOkmJApTcCZGHmkW6x4BvFBd30ZeAAO\nIfC8u96dAgMBAAECggEALBd7Wc2FrrzMZrV/3CNtg6KZziU6/C9EzhW4KOwR8hE6\nRbWt9PHL2y3w7LkD85YRDJNOPly66li4izp9KWyJOj8PmZkATC3/ei/RBgxghIah\nWDwfDI+88WsdtPl7BH1vTjdUA1SYOKM3N/h+iasC2Eea/ozmHOAVgqWOMep5UpXz\naRk6QFk59XTTX0GlNEKu1fmiMERgYxrXcba/euXSKUdPEUgJpfc77xoo59MGijpS\npbzq+m91tz+CpB+SItDyB2yClM3IrakibzjIl+OuPZq6DDJStqbZ5+yIsf5++yDt\nmdAwBp3PjUAQRlDaiS1zWe38FK4oU6GSR7kP5yRJwwKBgQDcyHUsw1L7tfBTeKL3\nKrClkO8BhlbKyHXvoum3Wo0X3XZoRTYqDOo9/wmBIIKhkCCrT4wUEsk3BEJxZwwH\nvd8bB9kNpoEkhVc3KXYXzBV//2ykPxCHEili3Ki5lsgIcLr/Jkdp1hpJMJNdHfsx\nacATotDJasFsVceChcafA2dRbwKBgQDFkAq4xg64rf4feNz3Nvg5d8aPU5X8OOlb\nf9/+upMlORLRLevpNxBIm/7k7EQp1tbwFN9e0dXIgmtI3HdQw/qQl6H5MDu7HKC/\nLvXBnL2eYJV3ncrCOUQ3c0dqdJe0dg1pGSTVObi20rHtdtNVxLua5SWFYOxJLUON\nedC1oC3yswKBgQCQJUB7bTsso+i4VrbvcKvMLQgekrrvwlldj5vP3groXesEwMtQ\nQQZeW7i02ajwGs15i4IFSQ6eIOXQbFrqYwja+RC5XNBMK9J5bVRwzQnse7NOuO3t\ngeoLZc2y/cKz4AX2t+NAMk0PI+vSi9SIumZxa4etOUn3lTmg+f5nqy01eQKBgQCy\noOKQ3sNAyFEVM7rW66TUkS+/N9xNCUg1qxFkyF1zMn+JEvRTEa5veFynr9iR2+bL\nfJPar++UNBe88VoQdKKBShjxrmLTc7cEJfme7ZGlOb7rQIirxAuh92GDJ5hjWroV\nZN6gzGfvJeIqy12aFx9plfau+kuqfnqt2I6VKhdhcQKBgQCnUyqDkYonsM6puxdK\nXcn8jwsJ/0kWmDshliKNtNmiD/N83hIaQo+bKyRfWupstI2CTT1LJXlwigAgqtxl\n/S/Xe71dMatrT6S/+nXPVipiruB9zuUOORjjW45tI3y1kM2FG/qakayEwGrS8yHK\n7GydlIz7i0hMbJSqviXjIs1lNQ==\n-----END PRIVATE KEY-----\n",
+  "client_email": "firebase-adminsdk-fbsvc@ecard-c8f61.iam.gserviceaccount.com",
+  "client_id": "106315868645432511914",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40ecard-c8f61.iam.gserviceaccount.com",
+  "universe_domain": "googleapis.com"
+}
